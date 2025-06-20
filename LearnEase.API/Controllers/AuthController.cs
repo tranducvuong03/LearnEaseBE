dcd00b3a371ad68a0f2c52334bf6fce3bf81e1e3
kiet-trann <kiettuantranle@gmail.com>
2025-06-20 21:52:59 +0700

    fix error login

--- LearnEase.API/Controllers/AuthController.cs
+++ LearnEase.API/Controllers/AuthController.cs
@@ -132,14 +132,14 @@ namespace LearnEase.API.Controllers
                 new Claim(ClaimTypes.Name, user.Username)
             };
 
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JwtSettings:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
             var token = new JwtSecurityToken(
-                issuer: _config["Jwt:Issuer"],
-                audience: _config["Jwt:Audience"],
+                issuer: _config["JwtSettings:Issuer"],
+                audience: _config["JwtSettings:Audience"],
                 claims: claims,
-                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_config["Jwt:ExpireMinutes"])),
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_config["JwtSettings:ExpireMinutes"])),
                 signingCredentials: creds
             );
 
