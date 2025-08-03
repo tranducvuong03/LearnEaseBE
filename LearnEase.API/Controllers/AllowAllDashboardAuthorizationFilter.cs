536272946ca1d101a1f0c962c15db72521721776
khoang1205 <khoandse172345@fpt.edu.vn>
2025-08-03 20:48:48 +0700

    fix authen hangfire

---
+++ LearnEase.API/Controllers/AllowAllDashboardAuthorizationFilter.cs
@@ -0,0 +1,14 @@
+﻿using Hangfire.Dashboard;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearnEase.API.Controllers
+{
+    public class AllowAllDashboardAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            return true; //  Cho phép tất cả mọi người truy cập dashboard
+        }
+    }
+
+}
